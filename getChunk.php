bf65f5b6af8af2d73c27375d6bc6f49c82df7924
Upasana-S <upasanasridhared@gmail.com>
2018-11-23 06:53:19 +0530

    -a

--- getChunk.php
+++ getChunk.php
@@ -1,9 +1,11 @@
 <?php
 	extract($_GET);
-	$chunk=200;
-	$pos=$count*$chunk;
+	$positions=[0,615,747,750,1740,1493,1653]
+	if(count<6){
+	$pos+=$positions[count];
 	$file=fopen("reactions_opt.html","r");
 	fseek($file,$pos);
-	$data=fread($file,$chunk);
-	echo $data;
+	$data=fread($file,$positions[count+1])
+	echo $pos
+	echo $data;}
 ?>
